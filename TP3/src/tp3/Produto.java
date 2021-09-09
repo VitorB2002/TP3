9c07e3317c4c8c37aee018ac330bb2252eb96698
VitorB2002 <vitorb.santos2002@gmail.com>
2021-09-09 16:43:10 -0300

    Implementação dos atributos e metodos getters e setters da classe abstrata "Produto"

--- TP3/src/tp3/Produto.java
+++ TP3/src/tp3/Produto.java
@@ -1,5 +1,45 @@
 package tp3;
 
 public abstract class Produto {
-
+	
+	private String modelo;
+	private String descrição;
+	private Double valor;
+	private String marca;
+	
+	//Getters e Setters
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+	public String getDescrição() {
+		return descrição;
+	}
+	
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	
+	public Double getValor() {
+		return valor;
+	}
+	
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
 }
